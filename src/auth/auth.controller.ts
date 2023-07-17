8890a913a89802f9bbb19770f59215fb052b28d3
wiss <yousfiwissem224@gmail.com>
2023-07-17 10:05:35 +0100

    admin

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SignUpDto } from './dto/signup.dto';
