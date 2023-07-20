69f13c23aba3d8afc53b21c41dc07750530352d7
Zakaria AKIL <zakaria.akil@telecom-paris.fr>
2023-07-20 17:00:56 +0200

    modelisation d'une transaction

--- Blockchain/contracts/certification.sol
+++ Blockchain/contracts/certification.sol
@@ -2,8 +2,16 @@
 pragma solidity ^0.8.0;
 contract EnvironmentalCertification {
     address public owner;
+    struct transation{
+        string date;
+        address  recyclingCenter;
+        string garbageType;
+        uint256  purchasedQuantity;
+        int price;
+    }
     mapping(address => bool) public certifiedCompanies;
-    mapping(address => uint256) public purchasedQuantities;
+    mapping(address => transation[]) public transactions;
+    mapping(address => uint256)public purchasedQuantities;
 
     modifier onlyOwner() {
         require(msg.sender == owner, "Only the contract owner can perform this action");
@@ -14,8 +22,13 @@ contract EnvironmentalCertification {
         owner = msg.sender;
     }
 
+    event companyCertified(
+        address _companyAddress
+    );
+
     function certifyCompany(address _companyAddress) public onlyOwner {
         certifiedCompanies[_companyAddress] = true;
+        emit companyCertified(_companyAddress);
     }
 
     function revokeCertification(address _companyAddress) public onlyOwner {
@@ -25,9 +38,10 @@ contract EnvironmentalCertification {
     function isCertified(address _companyAddress) public view returns (bool) {
         return certifiedCompanies[_companyAddress];
     }
-    function recordPurchasedQuantity(address _companyAddress, uint256 _quantity) public onlyOwner {
+    function recordTransaction(transation memory trans) public onlyOwner {
         //require(certifiedCompanies[_companyAddress], "Company must be certified to record purchases"); 
-        purchasedQuantities[_companyAddress] += _quantity;
+        transactions[trans.recyclingCenter].push(trans);
+        purchasedQuantities[trans.recyclingCenter] += trans.purchasedQuantity;
     }
     function getPurchasedQuantity(address _companyAddress) public view returns (uint256) {
         return purchasedQuantities[_companyAddress];
