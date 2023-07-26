38da8abb7f955a1c3acd9f340f9836b3ceca607c
Djo111 <youssef.weslati@ensi-uma.tn>
2023-07-26 11:40:06 +0100

    nothing

--- Front/lib/scenes/home_page.dart
+++ Front/lib/scenes/home_page.dart
@@ -25,77 +25,77 @@ class Add{
   });
 
   factory Add.fromJson(Map<String, dynamic>){
-    return Add(
-      username: json['username'],
-      description: json['description'],
-      img: json['img'],
-      user_category: json['user_category'],
-    );
+  return Add(
+  username: json['username'],
+  description: json['description'],
+  img: json['img'],
+  user_category: json['user_category'],
+  );
   }
 }
 
 calss _AddsState extends State<Adds>{
-  Future<List<Add> fetchAllAdds() async{
-    final response = await http.get(Uri.parse('http://localhost:3000/adds'));
+Future<List<Add> fetchAllAdds() async{
+  final response = await http.get(Uri.parse('http://localhost:3000/adds'));
 
-    if (response.statusCode == 200) {
-      // If the server returns a 200 OK response, parse the JSON.
-      List<dynamic> jsonList = jsonDecode(response.body);
-      List<Add> adds =
-          jsonList.map((json) => Offer.fromJson(json)).toList();
-      return adds;
-    } else {
-      // If the server did not return a 200 OK response, throw an exception.
-      throw Exception('Failed to load adds');
-    }
+  if (response.statusCode == 200) {
+    // If the server returns a 200 OK response, parse the JSON.
+    List<dynamic> jsonList = jsonDecode(response.body);
+    List<Add> adds =
+    jsonList.map((json) => Offer.fromJson(json)).toList();
+    return adds;
+  } else {
+    // If the server did not return a 200 OK response, throw an exception.
+    throw Exception('Failed to load adds');
   }
-
-  List<Add> AllAdds=[];
-  @override
-  void initState() {
-    super.initState();
-    fetchAllAdds().then((adds) {
-      setState(() {
-        AllAdds.addAll(adds);
-      });
-      print(AllAdds.length);
-    });
 }
 
- @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: const Color(0xFF171918),
-      body: ListView.builder(
-        itemCount: AllAdds.length,
-        itemBuilder: (context, index) {
-           return _buildPostContainer(adds[index]);
-        },
-      ),
-    );
+List<Add> AllAdds=[];
+@override
+void initState() {
+  super.initState();
+  fetchAllAdds().then((adds) {
+    setState(() {
+      AllAdds.addAll(adds);
+    });
+    print(AllAdds.length);
+  });
+}
+
+@override
+Widget build(BuildContext context) {
+  return Scaffold(
+    backgroundColor: const Color(0xFF171918),
+    body: ListView.builder(
+      itemCount: AllAdds.length,
+      itemBuilder: (context, index) {
+        return _buildPostContainer(adds[index]);
+      },
+    ),
+  );
 }
 
 Widget _buildPostContainer(Add post) {
-    return Card(
-      margin: EdgeInsets.all(8.0),
-      child: Column(
+  return Card(
+    margin: EdgeInsets.all(8.0),
+    child: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          ListTile(
-            leading: //CircleAvatar(
-              // You can use a NetworkImage or AssetImage based on your data.
-             // backgroundImage: NetworkImage(post.img),
-            //),
-            title: Text(post.username),
-          ),
-          Image.network(post.imageUrl),
-          Padding(
-            padding: EdgeInsets.all(16.0),
-            child: Text(post.description),
-          ),
-        ],
-      ),
-    );
-  }
+    ListTile(
+    leading: //CircleAvatar(
+    // You can use a NetworkImage or AssetImage based on your data.
+    // backgroundImage: NetworkImage(post.img),
+    //),
+    title: Text(post.username),
+  ),
+  Image.network(post.imageUrl),
+  Padding(
+  padding: EdgeInsets.all(16.0),
+  child: Text(post.description),
+  ),
+  ],
+  ),
+  );
+}
 }
 
